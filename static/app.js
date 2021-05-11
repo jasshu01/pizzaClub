693e5d3ef330852b3e0483dc1bab31f7a57ab214
Jasshu garg <67399702+jasshu01@users.noreply.github.com>
2021-05-11 12:42:49 +0530

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,120 +1,26 @@
-
-// const express = require("express");
-// const path = require("path");
-// // const fs = require("fs");
-// const app = express();
-// const port = 80;
-
-
-
-// // EXPRESS SPECIFIC STUFF
-// app.use('/static', express.static('static')) // For serving static files
-
-// // PUG SPECIFIC STUFF
-// app.set('view engine', 'pug') // Set the template engine as pug
-// app.set('views', path.join(__dirname, 'views')) // Set the views directory
-
-// // ENDPOINTS
-// app.get('/', (req, res) => {
-
-//   res.status(200).render('index.pug');
-// })
-
-// // app.post('/', (req, res) => {
-
-// //   console.log(req.body)
-// //   // console.log(req.body.name)
-// //   console.log(req.phone)
-// //   // console.log(req.body)
-// // })
-
-// app.post("/", (req, res) => {
-//   console.log(req.body);
- 
-// })
-
-
-// // START THE SERVER
-// app.listen(port, () => {
-//   console.log(`The application started successfully on port ${port}`);
-// });
-
-
-// var nodemailer = require('nodemailer');
-
-// var transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'jasshugarg0098@gmail.com',
-//     pass: 'Yashu@1801'
-//   }
-// });
-
-// var mailOptions = {
-//   from: 'jasshugarg0098@gmail.com',
-//   to: 'jasshugarg0098@gmail.com',
-//   subject: 'Sending Email using Node.js',
-//   text: 'That was easy!'
-// };
-
-// transporter.sendMail(mailOptions, function(error, info){
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Email sent: ' + info.response);
-//   }
-// });
-
-
-
-
-//-----------------------------------------
-
-
 const express = require("express");
 const path = require("path");
 var nodemailer = require('nodemailer');
-// const fs = require("fs");
-// var mongoose = require("mongoose");
 // const { mainModule } = require("process");
-// const bodyParser = require("body-parser");
-// mongoose.connect("mongodb://localhost/index", { useNewUrlParser: true, useUnifiedTopology: true })
 const app = express();
-const port = 80;
+var port=80;
+// var port = process.env.PORT || 8080;
 
-
-// const Excel = require('exceljs');
-// let workbook = new Excel.Workbook()
-// let worksheet = workbook.addWorksheet('Debtors');
-// worksheet.columns = [
-//     { header: 'Name', key: 'fname' },
-//     { header: 'Phone', key: 'mobile' },
-//     { header: 'Address', key: 'address' },
-//     { header: 'Order', key: 'order' }
-// ]
-
+// app.use(express.static(__dirname )
+app.use('/static', express.static('static')) // For serving static files
 
 // EXPRESS SPECIFIC STUFF
-app.use('/static', express.static('static')) // For serving static files
-// app.use(express.urlencoded())
+// app.use('/static', express.static('static')) // For serving static files
 app.use(express.urlencoded({ extended: true }))
 
-// PUG SPECIFIC STUFF
-// app.set('view engine', 'html') // Set the template engine as pug
 
 app.set('view engine', 'pug') // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) // Set the views directory
 
-// ENDPOINTS
-app.get('/', (req, res) => {
-    // const con = "This is the best content on the internet so far so use it wisely"
-    // const params = { 'title': 'PubG is the best game' }
-    res.status(200).render('index.pug');
-    // res.status(200).render('index.html', params);
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname+'/static/index.html'))
 })
-// count = 0;
-
-
 
 
 app.post("/", (req, res) => {
@@ -131,7 +37,8 @@ app.post("/", (req, res) => {
     var mailOptions = {
         from: 'jasshugarg0098@gmail.com',
         to: 'jasshugarg0098@gmail.com',
-        subject: req.body.name,
+        subject:`${req.body.name}` ,
+        // text: `${req.body.order}`
         text: `${req.body.name} \n ${req.body.phone} \n ${req.body.address}\n ${req.body.order}`
     };
 
@@ -145,10 +52,9 @@ app.post("/", (req, res) => {
 })
 
 
-// START THE SERVER
+// // START THE SERVER
 app.listen(port, () => {
-    console.log(`The application started successfully on port ${port}`);
-});
-
-
-
+        console.log(`The application started successfully on port ${port}`);
+    });
+    
+    
